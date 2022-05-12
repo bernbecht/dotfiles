cb588e2a6acb9c988949296015787db704e249a1
Bernardo Bechtold <1577809+Berhell@users.noreply.github.com>
2022-05-12 16:25:12 +0100

    adds process utility functions for shopify instances

--- zsh/config/functions.zsh
+++ zsh/config/functions.zsh
@@ -14,3 +14,30 @@ mk() {
 newosw() {
     spin up online-store-web --name "$1"
 }
+## Shopify
+base() {
+    local regex=/Shopify/(.+)/?
+    local command="$1"
+    local current_path=$(pwd) > /dev/null 2>&1
+    if [[ $current_path =~ $regex ]]; then
+      eval "${command} shopify--${match[1]}"
+    else
+      echo "Please run this script from a Shopify project"
+    fi
+}
+
+start() {
+  base "iso procs start"
+}
+
+stop() {
+  base "iso procs stop"
+}
+
+restart() {
+  base "iso procs restart"
+}
+
+update() {
+  base "/usr/bin/update"
+}
