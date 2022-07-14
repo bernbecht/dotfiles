9ad20c16e765a1b92fa29c04ae093dfc6c50299a
Bernardo Bechtold <bernardo.bechtold@shopify.com>
2022-07-14 14:00:19 +0200

    Adds ggpatch alias and variables

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -14,6 +14,7 @@ alias ggpurge='git branch | grep -v "master" | xargs git branch -D'
 alias ggdev='git checkout dev'
 alias ggmo='git merge origin/main'
 alias ggmend='git commit --amend --no-edit'
+alias ggpatch='ga . && ggmend && ggpush -f'
 # Shopify mobile
 alias rundroid='dev cd mobile && dev rn a'
 alias runios='dev cd mobile && dev rn i'
--- zsh/config/variables.zsh
+++ zsh/config/variables.zsh
@@ -20,4 +20,5 @@ export LC_ALL=$LANG
 export PATH="$HOME/.zsh/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/bin:$PATH"
-export PATH="${HOME}/Library/Android/sdk/tools:${HOME}/Library/Android/sdk/platform-tools:${PATH}"
+export PATH="${HOME}/Library/Android/sdk/tools:${HOME}/Library/Android/sdk/platform-tools:${PATH}:${HOME}/Library/Android/sdk/emulator:${PATH}"
+export PATH="/opt/homebrew/bin:$PATH"
