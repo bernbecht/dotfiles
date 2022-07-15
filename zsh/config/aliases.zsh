cc099432f8d8e1224e14103c4dee19b44a73cec9
Bernardo Bechtold <bernardo.bechtold@shopify.com>
2022-07-15 11:52:37 +0200

    Update aliases

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -14,7 +14,8 @@ alias ggpurge='git branch | grep -v "master" | xargs git branch -D'
 alias ggdev='git checkout dev'
 alias ggmo='git merge origin/main'
 alias ggmend='git commit --amend --no-edit'
-alias ggpatch='ga . && ggmend && ggpush -f'
+alias ggpatch='ga . && ggmend && ggpush -f
+alias ggcp='git cherry-pick'
 # Shopify mobile
 alias rundroid='dev cd mobile && dev rn a'
 alias runios='dev cd mobile && dev rn i'
