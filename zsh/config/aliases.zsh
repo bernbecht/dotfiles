23519cd83f063712306cd9ede45bac7758d17971
Bernardo Bechtold <bernardo.bechtold@shopify.com>
2022-07-18 10:44:42 +0200

    🐛 Adds missing quote to alias

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -1,22 +1,28 @@
 # ====================
 # Alias
 # ====================
+
 # General purpose
+# ----
 alias zshconfig="code  ~/.zshrc"
 alias p='cd ~/projects'
 alias nr='npm run'
 alias pp='cd ~/projects/personal'
 alias home='cd ~/'
+
 # Git
+# ----
 alias ggac='git add . && git commit'
 alias ggac:amend='git add . && git commit --amend'
 alias ggpurge='git branch | grep -v "master" | xargs git branch -D'
 alias ggdev='git checkout dev'
 alias ggmo='git merge origin/main'
 alias ggmend='git commit --amend --no-edit'
-alias ggpatch='ga . && ggmend && ggpush -f
+alias ggpatch='ga . && ggmend && ggpush -f'
 alias ggcp='git cherry-pick'
+
 # Shopify mobile
+# ---
 alias rundroid='dev cd mobile && dev rn a'
 alias runios='dev cd mobile && dev rn i'
 alias mob='dev cd mobile'
