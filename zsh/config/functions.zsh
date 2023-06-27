970f5abb303efa67521118425c120d86ed7c689c
Bernardo Bechtold <bernardo.bechtold@shopify.com>
2023-06-27 14:47:00 -0400

    Remove shopify functions

--- zsh/config/functions.zsh
+++ zsh/config/functions.zsh
@@ -14,35 +14,3 @@ mk() {
 ggrh() {
   git reset --hard HEAD~"$1"
 }
-## Shopify
-# creates new OSW spin instance
-newosw() {
-    spin up online-store-web --name "$1"
-}
-
-base() {
-    local regex=/Shopify/(.+)/?
-    local command="$1"
-    local current_path=$(pwd) > /dev/null 2>&1
-    if [[ $current_path =~ $regex ]]; then
-      eval "${command} shopify--${match[1]}"
-    else
-      echo "Please run this script from a Shopify project"
-    fi
-}
-
-start() {
-  base "iso procs start"
-}
-
-stop() {
-  base "iso procs stop"
-}
-
-restart() {
-  base "iso procs restart"
-}
-
-update() {
-  base "/usr/bin/update"
-}
